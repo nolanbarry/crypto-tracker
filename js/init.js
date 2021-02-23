e75c946370bc6507b13e993bd116a0e00ca6eb65
Nolan Barry <nolan.barry@live.com>
2021-02-22 22:20:08 -0700

    fetches top coins and displays on sidebar

--- js/init.js
+++ js/init.js
@@ -9,12 +9,3 @@ let head = document.head;
 head.appendChild(createStylesheetLink('styles'));
 head.appendChild(createStylesheetLink('header'));
 head.appendChild(createStylesheetLink('sidebar'));
-
-// resize sidebar height when page is resized
-function setSidebarHeight() {
-  let sidebar = document.getElementById('sidebar');
-  let header = document.getElementById('header');
-  sidebar.style.height = window.innerHeight - header.clientHeight + 'px';
-}
-document.body.addEventListener("resize", setSidebarHeight);
-setSidebarHeight(); // initalize
--- js/main.js
+++ js/main.js
@@ -1,9 +1,58 @@
 // on page load:
-let allListURL = "https://api.coingecko.com/api/v3/coins/list";
-let allCoins = {};
-fetch(allListURL)
-  .then(function(response) {
-    return response.json();
-  }).then(function(data) {
 
-  })
+/* https://www.coingecko.com/en/api */
+/* https://developers.coinranking.com/api/documentation/ */
+let coinRankKey = 'coinranking0c56a9b857f065ff49e44335d35cdb0b328a7c22e16b269d';
+let coinRankAllURL = 'https://api.coinranking.com/v2/coins';
+let coins;
+fetch(coinRankAllURL, {headers: { 'x-access-token': coinRankKey }})
+.then(function(response) {
+  return response.json();
+}).then(function(data) {
+  coins = data.data.coins;
+  let sidebar = document.getElementById('sidebar');
+  for(let coin of coins) {
+    sidebar.appendChild(createCoinSidebarListing(coin));
+  }
+});
+
+function element(type, options, ...children) {
+  let element = document.createElement(type);
+  for(let key of Object.keys(options)) {
+    element[key] = options[key];
+  }
+  for(let child of children) {
+    element.appendChild(child);
+  }
+  return element;
+}
+
+function determineCoinChangeClass(change) {
+  if (change < 0) return 'loss';
+  if (change > 0) return 'gain';
+  return 'no-change';
+}
+
+function truncateNumber(price, totalDigitCount) {
+  if (Number.isInteger(price)) return price;
+  ++totalDigitCount; // account for decimal as character
+  return price.slice(0, totalDigitCount + 1).toLocaleString(undefined,  { minimumFractionDigits: 2 });
+}
+
+function truncateDecimal(value, decimals) {
+  return Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals).toLocaleString(undefined,  { minimumFractionDigits: 2 });
+}
+
+function createCoinSidebarListing(coin) {
+  // heading
+  let name = element('h1', {className: 'sidebar-name', innerHTML: coin.name});
+  let abbrev = element('h1', {className: 'sidebar-abbrev', innerHTML: coin.symbol});
+  let heading = element('div', {className: 'container sidebar-heading'}, name, abbrev);
+  // subheading
+  let price = element('h2', {className: 'sidebar-price', innerHTML: truncateNumber(coin.price, 7)});
+  let percentChange = element('h2', {innerHTML: truncateDecimal(coin.change, 2), className: 'sidebar-change ' + determineCoinChangeClass(coin.change)});
+  let subheading = element('div', {className: 'container price-subheading'}, price, percentChange);
+  // combine into listing
+  let listing = element('div', {className: 'sidebar-listing'}, heading, subheading);
+  return listing;
+}
